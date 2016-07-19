032dd80680ff21a0eac87a5b956a7a189269bd63
Osborne Tunde <osbornetunde@gmail.com>
2016-07-19 06:48:37 +0100

    updated templatefile again

--- lwc/settings.py
+++ lwc/settings.py
@@ -125,10 +125,6 @@ SHARE_URL = "osbornelaunch.herokuapp.com/?ref="
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates')
-    )
-
 
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
